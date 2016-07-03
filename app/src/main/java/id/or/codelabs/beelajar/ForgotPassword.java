cfa57ea393dcd5592afb2c62d7a03f16b078cce4
Rizal Yogi Pratama <Rizal Yogi Pratama>
2016-07-03 22:09:55 +0700

    t03,t10

---
+++ app/src/main/java/id/or/codelabs/beelajar/ForgotPassword.java
@@ -0,0 +1,13 @@
+package id.or.codelabs.beelajar;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ForgotPassword extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+    }
+}
