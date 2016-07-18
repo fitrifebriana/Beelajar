6282d7350c897bd0eaf8fd227a63ad00cd8b29f6
Rizal Yogi Pratama <Rizal Yogi Pratama>
2016-07-18 12:31:49 +0700

    Splash ,Subject

---
+++ app/src/main/java/id/or/codelabs/beelajar/Splash.java
@@ -0,0 +1,36 @@
+package id.or.codelabs.beelajar;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread timer = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }finally {
+                    Intent i = new Intent(Splash.this,SignIn.class);
+                    startActivity(i);
+                    //finish();
+
+                }
+            }
+        };
+        timer.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
